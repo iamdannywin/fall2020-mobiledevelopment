5cdf72e5d83bcaa2b73be2d73781c8aced73c899
iamdannywin <70669570+iamdannywin@users.noreply.github.com>
2020-09-22 20:32:25 -0400

    assignment 2

--- week4/components/Sidebar.js
+++ week4/components/Sidebar.js
@@ -1,3 +1,4 @@
+
 import Component from './Component.js';
 
 class Sidebar extends Component {
@@ -9,11 +10,16 @@ class Sidebar extends Component {
   render () {
    
     let responseHTML = '';
-
+    responseHTML = responseHTML + '<ul>';
     this.props.menu.forEach( (item) => {
+    
       // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
+    responseHTML = responseHTML + `<li> ${item}</li>`;
     });
+    
+    responseHTML += '</ul>';
 
+    
     return responseHTML;
   }
 }
